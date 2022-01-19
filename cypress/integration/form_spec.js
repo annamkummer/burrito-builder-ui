515d04e0d1c5ef7c262a5bece2d492efa6924480
Anna Kummer <annamkummer@gmail.com>
2022-01-19 12:14:44 -0700

    Add more form tests

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -24,12 +24,32 @@ describe('Form user flow', () => {
         cy.get('.submit-btn').click()
     })
 
-    // it('After clicking Submit, if the order posted correctly, they should see their order appear in the orders section.', () => {
+    it('After clicking Submit, if the order posted correctly, they should see their order appear in the orders section.', () => {
+        cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+            statusCode: 200,
+            ok: true,
+            body: {
+                name: "Ok"
+            }
+        })
+        cy.get('.name-field').type('ABCD')
+        cy.get('button').first().click()
+        cy.get('.submit-btn').click()
+        cy.get('.order').contains('ABCD')
+    })
 
-    // })
-
-    // it('If the order did not post correctly, they should see an error message.', () => {
-
-    // })
+    it('If the order did not post correctly, they should see an error message.', () => {
+        cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+            statusCode: 500,
+            ok: false,
+            body: {
+                name: " Not ok"
+            }
+        })
+        cy.get('.name-field').type('ABCD')
+        cy.get('button').first().click()
+        cy.get('.submit-btn').click()
+        cy.get('.orders-section').contains('Oops!')
+    })
 
 })
